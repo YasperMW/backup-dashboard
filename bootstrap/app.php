f6c024bf8742462d42bdac107894dd489fefac99
YasperMW <144926957+YasperMW@users.noreply.github.com>
2025-07-19 07:56:49 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,11 +5,16 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+
+
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Illuminate\Console\Scheduling\Schedule $schedule){
+        $schedule->command('app:run-scheduled-backups')->everyMinute();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
